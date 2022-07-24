7b42ec9091ff84281015bef0990e7d6598255c07
Mykola Popeliuk <mycolapopelyuk@gmail.com>
2022-07-24 14:00:00 +0500

    fixed type of uploading file

--- data_extractor/data_savers/s3_saver.py
+++ data_extractor/data_savers/s3_saver.py
@@ -1,9 +1,9 @@
 import boto3
-
+import json
 
 class S3Saver:
     def __init__(self, bucket_name):
         self.bucket = boto3.resource('s3').Bucket(bucket_name)
 
     def save_data(self, data, file_path):
-        self.bucket.put_object(Key=file_path, Body=str(data).encode("utf-8"))
+        self.bucket.put_object(Key=file_path, Body=json.dumps(data).encode("utf-8"))
