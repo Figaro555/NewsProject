469fe4005e9c87aa76a73aa84e23c6c858b5f32d
Mykola Popeliuk <mycolapopelyuk@gmail.com>
2022-08-22 02:16:44 +0300

    added finding words in text

--- data_transformer/main.py
+++ data_transformer/main.py
@@ -1,6 +1,8 @@
 from pyspark.sql import SparkSession
 
+from config.words_to_find import words
 from loaders.s3_bucket import S3Bucket
+from transformers.rdd_transformer import RDDTransformer
 
 
 def main():
@@ -12,12 +14,15 @@ def main():
     ss.sparkContext.setLogLevel("ERROR")
 
     bucket = S3Bucket()
+    rdd_trans = RDDTransformer()
 
     files = ss.sparkContext.parallelize(bucket.get_files_in_folder())
     raw_data = files.flatMap(lambda file: bucket.load_data(file))
 
     if raw_data.count() == 0:
         return
+
+    data_with_finding = raw_data.map(lambda row: rdd_trans.find_words(row, words))
 
 
 if __name__ == '__main__':
